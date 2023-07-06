cca78a091ffc6231456462a4e5d1effec73bc027
jose-artur-matos90 <a19334@alunos.ipca.pt>
2023-07-06 22:23:16 +0100

    primeiro fatorial em tempos

---
+++ FactorialRec.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+unsigned int FactorialRec(unsigned int pn)
+{
+    if(pn == 0)
+        return 1;
+    else
+        return pn * FactorialRec(pn-1);
+}
+
+int main()
+{
+    unsigned int n;
+    printf("Enter a number: ");
+    scanf("%u", &n);
+    printf("Factorial of %u is %u\n", n, FactorialRec(n));
+    return 0;
+}
